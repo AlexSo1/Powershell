7bef7bc746f603ec71947e345f72a51503400bad
AlexSo1 <82595273+AlexSo1@users.noreply.github.com>
2021-04-15 17:12:17 +0300

    Add files via upload

---
+++ MigrateDHCP.ps1
@@ -0,0 +1,4 @@
+﻿$From = ''
+$To = ''
+Export-DhcpServer -File C:\Setup\DHCP\DHCPdata.xml -Leases -Force -Computer $From –Verbose
+Import-DhcpServer -File C:\Setup\DHCP\DHCPdata.xml -BackupPath C:\DHCP\ -Leases -ScopeOverwrite -Force -Computer $To –Verbose
